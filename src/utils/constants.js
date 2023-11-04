82b25f40dc25cdd0440a6295234156324e710d13
huzen-div <80887881+huzen-div@users.noreply.github.com>
2023-11-05 04:05:02 +0700

    init project

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+import movieCover from'../assets/image/movie-cover.jpg';
+import promptPay from'../assets/image/prompt-pay.jpg';
+
+export const MOVIE_SERVICE = 'https://api.themoviedb.org/3';
+export const MOVIE_API_KEY = 'ff62a111fdf3ac88d744c53158d47bb7'; //'8baba8ab6b8bbe247645bcae7df63d0d';
+export const MOVIE_COVER = movieCover;
+export const PROMPT_PAY_IMG = promptPay;
